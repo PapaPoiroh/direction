3e79205131bf9df205489c5ea6981daa3b010140
Pierre ESCOURROU <escourrou.pierre@gmail.com>
2025-07-28 23:39:15 +0200

    Update agenda.tsx

--- pages/agenda.tsx
+++ pages/agenda.tsx
@@ -1,8 +1,16 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../lib/supabaseClient";
+import CalendarView from "../components/CalendarView";
+
 export default function Agenda() {
+  const [tasks, setTasks] = useState<any[]>([]);
+  useEffect(() => {
+    supabase.from("tasks").select("*").then(({ data }) => setTasks(data || []));
+  }, []);
   return (
     <div>
       <h1>Agenda</h1>
-      <p>Consultez l’ordre du jour des prochaines réunions.</p>
+      <CalendarView tasks={tasks} />
     </div>
   );
 }
