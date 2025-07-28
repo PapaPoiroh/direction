6a9c80c51f70c967cbdbe6a2973dae20c92b4e09
Pierre ESCOURROU <escourrou.pierre@gmail.com>
2025-07-28 23:34:52 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,11 @@
 import '../styles/globals.css';
-import Layout from '../components/Layout';
 import type { AppProps } from 'next/app';
+import Layout from '../components/Layout';
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   );
 }
-
-export default MyApp;
